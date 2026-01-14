98b8dab1f26614f0cd328c3e3a2a3cd7153e52be
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2026-01-14 21:57:32 +0400

    added rate limiters to api

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource("projects", ProjectController::class);
 
