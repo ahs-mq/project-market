0c296fb3fbb53a4a5feb2bc1ea9cd77bd0892b4b
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2026-01-14 18:30:29 +0400

    Initial commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\project;
+use App\Http\Requests\StoreprojectRequest;
+use App\Http\Requests\UpdateprojectRequest;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreprojectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateprojectRequest $request, project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(project $project)
+    {
+        //
+    }
+}
